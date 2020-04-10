6c23031b2a74dd50c69a87ce4b02e4cb5889c7ef
Armando Hernandez Marin <armando.hernandez@infotec.mx>
2020-04-09 20:05:36 -0500

    test added

--- django/loggingapp/tests.py
+++ django/loggingapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_view(self):
+        reponse = self.client.get(reverse('logging'))
+        self.assertEqual(reponse.status_code, 201,
+                         "view should return 200 status code")
